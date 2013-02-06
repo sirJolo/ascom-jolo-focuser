f7b7071c5e9e19fdc57dfcb49ae0d8ea79ef44ad
jolo <drjolo@gmail.com>
2013-02-06 16:31:53 +0100

    Driver installer

--- VisualBasic/JoloFocuser/JoloFocuser/JoloFOC Test.js
+++ VisualBasic/JoloFocuser/JoloFocuser/JoloFOC Test.js
@@ -1,5 +1,5 @@
 //*** CHECK THIS ProgID ***
-var X = new ActiveXObject("JoloFOC.Focuser");
+var X = new ActiveXObject("ASCOM.JoloFOC.Focuser");
 WScript.Echo("This is " + X.Name + ")");
 // You may want to uncomment this...
 // X.Connected = true;
